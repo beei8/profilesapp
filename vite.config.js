3620c2ebe828ee21864a6daf67cf399095fc40c9
beei8 <beeines8@gmail.com>
2026-04-06 02:23:16 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: process.env.NODE_ENV === 'production' 
+          ? 'https://profilesapp-production.up.railway.app' 
+          : 'http://localhost:3001',
         changeOrigin: true
       }
     }
